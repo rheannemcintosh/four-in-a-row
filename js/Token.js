e18b8b48d765feaea2efd43994f9416b2e188bc2
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-28 19:03:22 +0000

    Render the Tokens

--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,16 @@ class Token {
         this.tokenID = `token-${index}-${player.id}`;
         this.played = false;
     }
+
+    htmlToken () {
+
+    }
+
+    drawHTMLToken () {
+        const token = document.createElement('div');
+        document.getElementById('game-board-underlay').appendChild(token);
+        token.setAttribute('id', this.id);
+        token.setAttribute('class', 'token');
+        token.style.backgroundColor = this.owner.color;
+    }
 }
