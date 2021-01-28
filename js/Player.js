8c3a67a16573c2405eb6b31a131fe427fa7ce7a0
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-28 20:00:43 +0000

    Get the active Token

--- js/Player.js
+++ js/Player.js
@@ -30,5 +30,12 @@ class Player {
     get unusedTokens () {
         return this.tokens.filter(token => !token.dropped);
     }
+
+    /**
+     * Gets the active token by returning the first token in the array of unused tokens.
+     * @return {Object} First token object in the array of unused tokens.
+     */
+    get activeToken () {
+        return this.unusedTokens[0];
     }
 }
