c9fe4a6ff3313320b8a1248b6246682f51ce14ef
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-28 23:42:13 +0000

    Start the Game

--- js/Game.js
+++ js/Game.js
@@ -36,7 +36,8 @@ class Game {
      * Gets game ready for play
      */
     startGame () {
-
+        this.board.drawHTMLBoard();
+        this.activePlayer.activeToken.drawHTMLToken();
+        this.ready = true;
     }
-    
 }
