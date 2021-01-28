f8d07e236880196e5ba0127cbddb9ccd2f01fce5
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-28 18:52:02 +0000

    Rendering the Spaces

--- js/Space.js
+++ js/Space.js
@@ -3,8 +3,27 @@ class Space {
     constructor (x, y) {
         this.x = x;
         this.y = y;
+        this.diameter = 76;
+        this.radius = this.diameter / 2;
         this.id = `space-${x}-${y}`;
         this.token = null;
+    }
+
+    drawSVGSpace () {
+        
+        // Create the SVG Element
+        const svgSpace = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        
+        // Set the Attrtibutes
+        svgSpace.setAttributeNS(null, "id", this.id);
+        svgSpace.setAttributeNS(null, "cx", (this.x * this.diameter) + this.radius);
+        svgSpace.setAttributeNS(null, "cy", (this.y * this.diameter) + this.radius);
+        svgSpace.setAttributeNS(null, "r", this.radius - 8);
+        svgSpace.setAttributeNS(null, "fill", "black");
+        svgSpace.setAttributeNS(null, "stroke", "none");
+        
+        //Attach the Element to the DOM
+        document.getElementById("mask").appendChild(svgSpace);   
     }
     
 }
