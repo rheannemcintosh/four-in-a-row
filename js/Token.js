7597bd0bda8d912a8dd5c09d24ada7f00b9ce7e8
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-29 21:52:29 +0000

    Allow token to move left and right

--- js/Token.js
+++ js/Token.js
@@ -38,4 +38,25 @@ class Token {
         token.setAttribute('class', 'token');
         token.style.backgroundColor = this.player.colour;
     }
+
+    /** 
+     * Moves html token one column to left.
+     */
+    moveLeft () {
+        if (this.columnLocation > 0) {
+            this.htmlToken.style.left = this.offsetLeft - 76;
+            this.columnLocation -= 1;
+        }
+    }
+
+    /** 
+     * Moves html token one column to right.
+     * @param   {number}    columns - number of columns in the game board
+     */
+    moveRight (columns) {
+        if (this.columnLocation < columns - 1) {
+            this.htmlToken.style.left = this.offsetLeft + 76;
+            this.columnLocation += 1;
+        }
+    }
 }
