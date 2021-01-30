cc7e3982132cf502ffeed369078363bb8a583c6d
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-01-30 22:51:12 +0000

    Add function to switch the which player is playing

--- js/Game.js
+++ js/Game.js
@@ -135,5 +135,23 @@ class Game {
 
         return win;
     }
+
+    /**
+     * Switches active player
+     */
+    switchPlayers () {
+        for (let player of this.players) {
+            player.active = player.active === true ? false : true;
+        }
+    }
+
+    /** 
+     * Displays game over message.
+     * @param {string} message - Game over message.      
+     */
+    gameOver (message) {
+        document.getElementById('game-over').style.display = 'block';
+        document.getElementById('game-over').textContent = message;
+    }
     
 }
